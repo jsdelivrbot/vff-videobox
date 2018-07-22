ab76815cd3ffb8857cbbe61085d1001909ae4c8f
dok <dumansky@gmail.com>
2018-07-22 21:26:09 +0300

    signaling server

--- src/videobox.js
+++ src/videobox.js
@@ -111,7 +111,8 @@ export default class VideoBox extends HTMLElement {
 
         // if(inController) return;
 
-        var signalingServer = self.signalingServer || "https://rtc.medialooks.com:8889";
+        // var signalingServer = self.signalingServer || "https://rtc.medialooks.com:8889";
+        var signalingServer = self.signalingServer || "http://rtc.videoflow.io";
         var room = url.split("/")[0];
         var targetId = url.split("/")[1];
 
